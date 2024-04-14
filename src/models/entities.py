71a1a05cb8ff4892d9b25ea7229b682eb47473c5
= <=>
2024-04-13 23:52:49 -0300

    BANCO DE DADOS ONLINE

--- src/models/entities.py
+++ src/models/entities.py
@@ -1,12 +1,13 @@
 EMPLOYEE = """employee (
                 id SERIAL PRIMARY KEY,
                 name VARCHAR(100),
-                gender VARCHAR(10),
+                gender VARCHAR(20),
                 age INTEGER,
+                race VARCHAR(20),
                 study VARCHAR(20),
                 state VARCHAR(20),
                 time NUMERIC(5, 2),
-                department VARCHAR(20),
+                department VARCHAR(40),
                 seniority varchar(20),
                 entry_age NUMERIC(5, 2)
             )"""
--- src/models/table.py
+++ src/models/table.py
@@ -2,14 +2,14 @@
 
 def create_table(conn: object,cursor: object, sql:str) -> bool:
     result = False
-    try:
+    #try:
         # Executando a consulta SQL para criar uma tabela
         
-        cursor.execute(f"CREATE TABLE IF NOT EXISTS {sql}")
-        conn.commit()
-        result = True
-    except:
-        pass
+    cursor.execute(f"CREATE TABLE IF NOT EXISTS {sql}")
+    conn.commit()
+    result = True
+    #except:
+    #    pass
     
     return result
 
@@ -28,15 +28,15 @@ def delete_table(conn: object,cursor: object, table_name:str) -> bool:
     return result
 
 
-def insert_table(conn: object, cursor: object, table_name:str, values:str)-> bool:
+def insert_table(conn: object, cursor: object, table_name:str, values:tuple)-> bool:
     result = False
-    try:
+    #try:
         # Executando a consulta SQL para deletar a tabela
-        delete_table_query = f'INSERT INTO {table_name} VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
-        cursor.execute(delete_table_query, values)
-        conn.commit()
-        result = True
-    except:
-        pass
+    delete_table_query = f'INSERT INTO {table_name} VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
+    cursor.execute(delete_table_query, values)
+    conn.commit()
+    result = True
+    #except:
+    #    pass
     
     return result
